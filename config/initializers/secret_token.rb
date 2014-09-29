8c984c5303a04223e305128c484fc25d906238a4
Erik Dodge <dodgees2@gmail.com>
2014-09-29 15:39:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DodgeTechExercise::Application.config.secret_key_base = '66ee9bd0692df2e655dbc32c7a1abb86cee1b94a7ac94e3aefec9cac1851bccd3322f58407e364c13e88c8bffd94324b67e4befebe266a3591c3fbb3231a1337'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DodgeTechExercise::Application.config.session_store :cookie_store, key: '_dodge_tech_exercise_session'
